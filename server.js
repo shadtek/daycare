5af662c74c4f37b09f449e45a9c36f412677bce1
Spencer Shattuck <spencer.shattuck@gmail.com>
2017-04-18 21:20:36 -0600

    Add some code to node server to hopefully make it so heroku works.

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ var port = 8080;
 // setup the app middlware
 app.use(express.static('./public'));
 
+app.all('*', function(req, res, next) {
+    res.sendFile('/public/index.html', { root: __dirname });
+});
+
 app.listen(port, function() {
 	console.log('Server up and running on port: ', port);
 });
