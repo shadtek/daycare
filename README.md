f553bfdcaefb8a7fbe989329c74323b57cfad464
Spencer Shattuck <spencer.shattuck@gmail.com>
2017-05-10 13:39:28 -0600

    Facebook login works, but I don't like it yet.

--- README.md
+++ README.md
@@ -7,27 +7,6 @@ Logo: http://img11.deviantart.net/3989/i/2016/167/1/1/uoplogocol2_by_stabrina-da
 
 Stylus watcher: stylus -w -u nib css/main.styl -o css/main.css
 
-Angular Wireframe:
-Nav - main.html
-	Home - home/home.html
-		Logo
-		Call now
-		Masonry filters
-		Masonry cards
-			Main image
-			Big text
-			Small text
-			Text area
-			Carousel
-	Pricing - pricing/pricing.html
-		Text area
-	Food menu - food/food-menu.html
-		Table
-	Calendar - calendar/calendar.html
-		docs.html format with horizontal card style
-Contact Us - main.html
-	Tap target http://materializecss.com/feature-discovery.html
-
 Sprints:
 Clean up all card title wrappers.
 Images can be expanded.
--- server.js
+++ server.js
@@ -1,14 +1,49 @@
-var express = require('express');
+const express = require('express');
+const session = require('express-session')
+const passport = require('passport');
+const passportConfig = require('./config')
+const FacebookStrategy = require('passport-facebook').Strategy;
 
-var port = process.env.PORT || 8080;
-var app = express();
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(session({ secret: 'some-random-string' }));
+
+passport.use(new FacebookStrategy({
+	clientID: passportConfig.clientID,
+	clientSecret: passportConfig.clientSecret,
+	callbackURL: "http://localhost:3000/admin/callback",
+}, function(token, refreshToken, profile, done){
+	return done(null, profile);
+}));
+
+app.get('/admin', passport.authenticate('facebook'));
+app.get('/admin/callback', passport.authenticate('facebook', {
+	successRedirect: '/',
+	failureRedirect: '/admin'
+}), function(req, res){
+	console.log(req.session);
+})
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
 
 app.use(express.static(__dirname + '/public'));
 
 app.all('*', function(req, res, next) {
-    res.sendFile('/public/index.html', { root: __dirname });
+	res.sendFile('/public/index.html', { root: __dirname });
 });
 
 app.listen(port, function(err){
   console.log('connected at ', port);
 });
+
+
